12782e1bf2b72d8fafb61da1d6045dc5adee3cb9
daming-Z <daming17@163.com>
2024-10-27 22:53:10 +0800

    Site updated: 2024-10-27 22:53:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/27/666/","2024/10/27/hello-world - 副本 - 副本/","2024/10/27/hello-world - 副本/","2024/10/20/hello-world/","2024/10/27/hello/"];function toRandomPost(){
+var posts=["2024/10/27/hello-world - 副本 - 副本/","2024/10/27/666/","2024/10/27/hello-world - 副本/","2024/10/20/hello-world/","2024/10/27/hello/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
